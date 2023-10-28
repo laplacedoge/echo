c1ea6ba330e7b9960eeb67e9045459cbe40c2758
Alex Chen <laplacedoge@gmail.com>
2023-10-28 11:27:37 +0000

    Add more structures and function prototypes

--- echo.h
+++ echo.h
@@ -5,13 +5,85 @@ typedef enum _EcoRes {
 
     EcoRes_Err              = -1,
     EcoRes_NoMem            = -2,
+
+    EcoRes_NotFound         = -3,
+
+    EcoRes_BadOpt           = -4,
+
+    EcoRes_ReachEnd         = -5,
 } EcoRes;
 
+typedef enum _EcoHttpVer {
+    EcoHttpVer_0_9,
+    EcoHttpVer_1_0,
+    EcoHttpVer_1_1,
+} EcoHttpVer;
+
+typedef enum _EcoHttpMethod {
+    EcoHttpMethod_Get,
+    EcoHttpMethod_Post,
+    EcoHttpMethod_Head,
+} EcoHttpMethod;
+
 typedef enum _EcoOpt {
-    EcoOpt_HttpHeader
+    EcoOpt_Url,
+    EcoOpt_Method,
+    EcoOpt_Verion,
+    EcoOpt_Header,
+    EcoOpt_BodyBuf,
+    EcoOpt_BodyLen,
+
+    EcoOpt_Request,
+    EcoOpt_ChanHookArg,
+    EcoOpt_ChanOpenHook,
+    EcoOpt_ChanCloseHook,
+    EcoOpt_ChanSetOptHook,
+    EcoOpt_ChanReadHook,
+    EcoOpt_ChanWriteHook,
+
+    EcoOpt_BodyHookArg,
+    EcoOpt_BodyWriteHook,
 } EcoOpt;
 
 
+
+typedef void * EcoArg;
+
+
+typedef enum _EcoChanOpt {
+    EcoChanOpt_SyncReadWrite,
+    EcoChanOpt_ReadWriteTimeout,
+} EcoChanOpt;
+
+
+
+typedef struct _EcoKvp {
+    uint32_t placeholder;
+} EcoKvp;
+
+typedef struct _EcoHdrTab {
+    uint32_t placeholder;
+} EcoHdrTab;
+
+
+
+void EcoHdrTab_Init(EcoHdrTab *tab);
+
+EcoHdrTab *EcoHdrTab_New(void);
+
+void EcoHdrTab_Deinit(EcoHdrTab *tab);
+
+void EcoHdrTab_Del(EcoHdrTab *tab);
+
+EcoRes EcoHdrTab_Add(EcoHdrTab *tab, const char *key, const char *val);
+
+EcoRes EcoHdrTab_Drop(EcoHdrTab *tab, const char *key);
+
+EcoRes EcoHdrTab_Clear(EcoHdrTab *tab);
+
+EcoRes EcoHdrTab_Find(EcoHdrTab *tab, const char *key, EcoKvp *kvp);
+
+EcoRes EcoHdrTab_Alter(EcoHdrTab *tab, const char *key, const char *val);
 
 typedef struct _EcoHttpReq {
     uint32_t placeholder;
@@ -31,14 +103,84 @@ typedef struct _EcoHttpCli {
 
 
 
+typedef struct _EcoChanAddr {
+    uint8_t addr[4];
+    uint16_t port;
+} EcoChanAddr;
+
+
+
+typedef EcoRes (*EcoChanOpenHook)(EcoChanAddr *addr, EcoArg arg);
+
+typedef EcoRes (*EcoChanCloseHook)(EcoArg arg);
+
+/**
+ * @brief User defined channel option setting hook function.
+ * 
+ * @param opt Option to set.
+ * @param arg Option data to set.
+ * @param hookArg Extra user data which can be set by option `EcoOpt_ChanHookArg`.
+ */
+typedef EcoRes (*EcoChanSetOptHook)(EcoChanOpt opt, EcoArg arg, EcoArg hookArg);
+
+/**
+ * @brief User defined channel read hook function.
+ * 
+ * @param buf Buffer to store read data.
+ * @param len Data length to read.
+ * @param arg Extra user data which can be set by option `EcoOpt_ChanHookArg`.
+ * 
+ * @return The actual length of the read data.
+ *         `EcoRes_ReachEnd` indicates the current channel has been closed.
+ *         Other negative numbers represent corresponding errors.
+ */
+typedef int (*EcoChanReadHook)(void *buf, int len, EcoArg arg);
+
+/**
+ * @brief User defined channel write hook function.
+ * 
+ * @param buf Buffer to store written data.
+ * @param len Data length to write.
+ * @param arg Extra user data which can be set by option `EcoOpt_ChanHookArg`.
+ * 
+ * @return The actual length of the written data.
+ *         `EcoRes_ReachEnd` indicates the current channel has been closed.
+ *         Other negative numbers represent corresponding errors.
+ */
+typedef int (*EcoChanWriteHook)(const void *buf, int len, EcoArg arg);
+
+
+
+/**
+ * @brief User defined body write hook function.
+ * 
+ * @note When arg `off` equals 0, it indicates this is the first packet.
+ *       When arg `len` equals 0, it indicates this is the last packet.
+ * 
+ * @param off Data offset.
+ * @param buf Buffer to store written data.
+ * @param len Data length to write.
+ * @param arg Extra user data which can be set by option `EcoOpt_BodyHookArg`.
+ * 
+ * @return The actual length of the written data.
+ *         Negative numbers represent corresponding errors.
+ */
+typedef int (*EcoBodyWriteHook)(int off, const void *buf, int len, EcoArg arg);
+
 
 
 EcoHttpReq *EcoHttpReq_New(void);
 
 void EcoHttpReq_Del(EcoHttpReq *req);
 
+EcoRes EcoHttpReq_SetOpt(EcoHttpReq *req, EcoOpt opt, EcoArg arg);
+
 
 
 EcoHttpCli *EcoHttpCli_New(void);
 
 void EcoHttpCli_Del(EcoHttpCli *cli);
+
+EcoRes EcoHttpCli_SetOpt(EcoHttpCli *cli, EcoOpt opt, EcoArg arg);
+
+EcoRes EcoHttpCli_Issue(EcoHttpCli *cli);
--- test/test.c
+++ test/test.c
@@ -1,35 +1,182 @@
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
 #include <stdio.h>
 
 #include "echo.h"
 
+#define SAVE_FILE_PATH  "save.bin"
 
-int main(int argc, char *argv[]) {
-    EcoHttpCli *cli = NULL;
-    EcoHttpReq *req = NULL;
-    EcoHttpRsp *rsp = NULL;
-    EcoRes res;
+EcoRes ecoChanOpenHook(EcoChanAddr *addr, void *arg) {
+    struct sockaddr_in srvAddr;
+    int sockFd;
+    int ret;
 
-    req = EcoHttpReq_New();
-    if (req == NULL) {
-        return EXIT_FAILURE;
+    sockFd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sockFd == -1) {
+        return EcoRes_Err;
     }
 
-    res = EcoHttpReq_SetOpt();
+    srvAddr.sin_family = AF_INET;
+    srvAddr.sin_port = htons(addr->port);
+    memcpy(&srvAddr.sin_addr, &addr->addr, 4);
 
+    ret = connect(sockFd, (struct sockaddr *)&srvAddr, sizeof(srvAddr));
+    if (ret != 0) {
+        close(sockFd);
 
+        return EcoRes_Err;
+    }
 
-    return EXIT_SUCCESS;
+    *(int *)arg = sockFd;
+
+    return EcoRes_Ok;
+}
+
+int ecoChanReadHook(void *buf, int len, void *arg) {
+    int sockFd;
+    int ret;
+
+    sockFd = *(int *)arg;
+
+    ret = (int)read(sockFd, buf, (size_t)len);
+    if (ret <= 0) {
+        if (ret == 0) {
+            return EcoRes_ReachEnd;
+        }
+
+        return EcoRes_Err;
+    }
+
+    return ret;
+}
+
+int ecoChanWriteHook(const void *buf, int len, void *arg) {
+    int sockFd;
+    int ret;
+
+    sockFd = *(int *)arg;
+
+    ret = (int)write(sockFd, buf, (size_t)len);
+    if (ret <= 0) {
+        if (ret == 0) {
+            return EcoRes_ReachEnd;
+        }
+
+        return EcoRes_Err;
+    }
+
+    return ret;
+}
+
+EcoRes ecoChanCloseHook(void *arg) {
+    int sockFd;
+    int ret;
+
+    sockFd = *(int *)arg;
+
+    ret = close(sockFd);
+    if (ret != 0) {
+        return EcoRes_Err;
+    }
+
+    return EcoRes_Ok;
 }
 
 
 
+int ecoBodyWriteHook(int off, const void *buf, int len, void *arg) {
+    int fileFd;
+    int ret;
+
+    fileFd = *(int *)arg;
+
+    if (off == 0) {
+        fileFd = open(SAVE_FILE_PATH, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+        if (fileFd < 0) {
+            return EcoRes_Err;
+        }
+
+        *(int *)arg = fileFd;
+    }
+
+    if (len > 0) {
+        ret = (int)write(fileFd, buf, (size_t)len);
+        if (ret != len) {
+            return EcoRes_Err;
+        }
+    }
+
+    if (len == 0) {
+        fileFd = *(int *)arg;
+
+        close(fileFd);
+    }
+
+    return len;
+}
 
 
 
+int main(int argc, char *argv[]) {
+    char bodyBuf[] = "{\"version\":\"1\",\"config\":{\"sleepTimeout\":60}}";
+    size_t bodyLen = sizeof(bodyBuf) - 1;
+    int sockFd;
+    int fileFd;
+    EcoHttpCli *cli = NULL;
+    EcoHttpReq *req = NULL;
+    EcoHdrTab *tab = NULL;
+    EcoRes res;
 
+    req = EcoHttpReq_New();
 
+    res = EcoHttpReq_SetOpt(req, EcoOpt_Url, "https://192.168.8.72:22120");
 
+    res = EcoHttpReq_SetOpt(req, EcoOpt_Verion, (EcoArg)EcoHttpVer_1_1);
 
+    res = EcoHttpReq_SetOpt(req, EcoOpt_Method, (EcoArg)EcoHttpMethod_Post);
 
+    /* Create HTTP header table. */
+    tab = EcoHdrTab_New();
+    res = EcoHdrTab_Add(tab, "Connection", "keep-alive");
+    res = EcoHdrTab_Add(tab, "Content-Type", "application/json");
+    res = EcoHdrTab_Add(tab, "Accept-Encoding", "gzip");
 
+    /* Set HTTP header field of HTTP request. */
+    res = EcoHttpReq_SetOpt(req, EcoOpt_Header, (EcoArg)tab);
+
+    /* Set POST body field. */
+    res = EcoHttpReq_SetOpt(req, EcoOpt_BodyBuf, (EcoArg)&bodyBuf);
+    res = EcoHttpReq_SetOpt(req, EcoOpt_BodyLen, (EcoArg)bodyLen);
+
+    /* Create HTTP client. */
+    cli = EcoHttpCli_New();
+
+    /* Set channel hook functions of HTTP client. */
+    res = EcoHttpCli_SetOpt(cli, EcoOpt_ChanHookArg, &sockFd);
+    res = EcoHttpCli_SetOpt(cli, EcoOpt_ChanOpenHook, ecoChanOpenHook);
+    res = EcoHttpCli_SetOpt(cli, EcoOpt_ChanCloseHook, ecoChanCloseHook);
+    res = EcoHttpCli_SetOpt(cli, EcoOpt_ChanReadHook, ecoChanReadHook);
+    res = EcoHttpCli_SetOpt(cli, EcoOpt_ChanWriteHook, ecoChanWriteHook);
+
+    /* Set body write hook function of HTTP client. */
+    res = EcoHttpCli_SetOpt(cli, EcoOpt_BodyHookArg, &fileFd);
+    res = EcoHttpCli_SetOpt(cli, EcoOpt_BodyWriteHook, ecoBodyWriteHook);
+
+    /* Set HTTP request of HTTP client. */
+    res = EcoHttpCli_SetOpt(cli, EcoOpt_Request, req);
+
+    /* Issue HTTP request. */
+    res = EcoHttpCli_Issue(cli);
+
+    /* Delete HTTP client. */
+    EcoHttpCli_Del(cli);
+
+    return EXIT_SUCCESS;
+}
